9bc95008aee99ba16e22228388d807634976de37
Shreyasbhandesh <shreyasbhandesh@gmail.com>
2025-04-26 11:56:20 +0530

    Update index.js
    Signed-off-by: Shreyasbhandesh <shreyasbhandesh@gmail.com>

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.get('/', (req, res) => res.send('Hello from Jenkins Pipeline!'));
-app.listen(PORT, () => console.log(`App running on port ${PORT}`));
+app.listen(PORT, () => console.log(App running on port ${PORT}));
